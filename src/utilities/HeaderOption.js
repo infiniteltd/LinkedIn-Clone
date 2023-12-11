0817cb6aff57311f78166e2ef578c7d1a9e193c5
infiniteltd <chrissamps24@gmail.com>
2023-12-11 11:08:16 +0100

    updated headerOption function and app title

--- src/utilities/HeaderOption.js
+++ src/utilities/HeaderOption.js
@@ -7,13 +7,18 @@ import { selectUser } from '../features/userSlice';
 function HeaderOption({ avatar, Icon, title, onClick }) {
     const user = useSelector(selectUser);
 
+    // Checking if user exists before accessing its properties
     const displayNameInitial = user?.displayName ? user.displayName[0] : '';
+    const userPhotoUrl = user?.photoUrl || ''; // Default to an empty string if photoUrl is not available
 
     return (
         <div onClick={onClick} className='headerOption'>
             {Icon && <Icon className='headerOption__icon' />}
             {avatar && (
-                <Avatar className='headerOption__icon' >{displayNameInitial}</Avatar>
+                <Avatar className='headerOption__icon' src={userPhotoUrl}>
+                    {/* Display displayNameInitial as a tooltip */}
+                    <span title={user?.displayName}>{displayNameInitial}</span>
+                </Avatar>
             )}
             <h3 className='headerOption__title'>{title}</h3>
         </div>
